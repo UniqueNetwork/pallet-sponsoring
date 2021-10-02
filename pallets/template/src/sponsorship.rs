987c8e1adf34a8581c3b045b75aa1537db954c77
str-mv <strmax20@gmail.com>
2021-10-02 13:07:15 +0300

    Sponsoring demo node

---
+++ pallets/template/src/sponsorship.rs
@@ -0,0 +1,47 @@
+use crate::{
+	Config, Call, 	
+	// CollectionById, CreateItemBasket, VariableMetaDataBasket,
+	// ReFungibleTransferBasket, FungibleTransferBasket, NftTransferBasket, CreateItemData,
+	// CollectionMode,
+};
+use core::marker::PhantomData;
+use sponsorship_primitives::SponsorshipHandler;
+use frame_support::{
+	traits::{IsSubType},
+//	storage::{StorageMap, StorageDoubleMap},
+};
+// use nft_data_structs::{
+// 	TokenId, CollectionId, NFT_SPONSOR_TRANSFER_TIMEOUT, REFUNGIBLE_SPONSOR_TRANSFER_TIMEOUT,
+// 	FUNGIBLE_SPONSOR_TRANSFER_TIMEOUT,
+// };
+
+pub struct NftSponsorshipHandler<T>(PhantomData<T>);
+
+#[allow(unused_variables)]
+impl<T: Config> NftSponsorshipHandler<T> {
+	pub fn withdraw(
+		who: &T::AccountId,
+		something: &u32,
+		who_will_pay: &T::AccountId,
+	) -> Option<T::AccountId> {
+
+		Some(who_will_pay.clone())
+	}
+
+}
+
+impl<T, C> SponsorshipHandler<T::AccountId, C> for NftSponsorshipHandler<T>
+where
+	T: Config,
+	C: IsSubType<Call<T>>,
+{
+	fn get_sponsor(who: &T::AccountId, call: &C) -> Option<T::AccountId> {
+
+		match IsSubType::<Call<T>>::is_sub_type(call)? {
+			Call::do_something(something, who_will_pay) => {
+				Self::withdraw(who, something, who_will_pay)
+			}
+			_ => None,
+		}
+	}
+}
